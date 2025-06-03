c3829d4c677a5f5b82006ea057457350963bf9e4
Brian Carroll <brian.carroll@holman.com>
2025-06-03 13:55:18 -0400

    subtle changes

--- tools/schema_to_markdown.py
+++ tools/schema_to_markdown.py
@@ -5,7 +5,82 @@ additional information."""
 
 import json
 import os
+from pydantic import BaseModel, Field
 from pathlib import Path
+
+
+class Framework(BaseModel):
+    """Model representing a framework with its title and external references."""
+    id: str = Field(...,
+                     description="The unique identifier for the framework.")
+    ref: str = Field(...,
+                      description="The reference identifier for the framework.")
+    author: str = Field(..., description="The author of the framework.")
+    category: list[str] = Field(
+        default_factory=list, description="Categories associated with the framework.")
+    created_on: str = Field(...,
+                            description="The date when the framework was created.")
+    description: str = Field(...,
+                             description="A brief description of the framework.")
+    title: str = Field(..., description="The title of the framework.")
+    external_references: list[dict] = Field(
+        default_factory=list,
+        description="A list of external references related to the framework."
+    )
+    friendly_name: str = Field(
+        default=None,
+        description="A user-friendly name for the framework, if applicable."
+    )
+    title: str = Field(
+        default=None,
+        description="The title of the framework, if applicable."
+    )
+    unique_id: str = Field(
+        default=None,
+        description="A unique identifier for the framework, if applicable."
+    )
+    
+    @property
+    def _id(self) -> str:
+        """Returns the unique identifier for the framework."""
+        return self.id
+    
+    @property
+    def _ref(self) -> str:
+        """Returns the reference identifier for the framework."""
+        return self.ref
+    
+    def self_url(self) -> str:
+        """Returns the URL to the framework's self-reference."""
+        return f"frameworks/{self._id}.md"
+
+
+def load_frameworks() -> dict:
+    """Loads all the frameworks from the JSON files in the 'frameworks' directory."""
+    current_dir = Path(os.getcwd())
+
+    # Load from the spec/frameworks directory
+    frameworks_dir = current_dir / 'spec' / 'frameworks'
+    frameworks = {}
+
+    if not frameworks_dir.exists():
+        print(f"Frameworks directory '{frameworks_dir}' does not exist.")
+        return frameworks
+            
+    # Convert each JSON file in the frameworks directory to a Framework model
+    for f in frameworks_dir.glob('*.json'):
+        with open(f, 'r', encoding='utf-8') as file:
+            try:
+                framework_data = json.load(file)
+                framework = Framework(**framework_data)
+                frameworks[framework._id] = framework.model_dump()
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON from {f}: {e}")
+            except Exception as e:
+                print(f"Error processing {f}: {e}")
+        
+    return frameworks
+
 
 def convert_schema_to_markdown(schema_path: Path) -> str:
     """Converts a schema JSON file to Markdown format."""
@@ -15,6 +90,47 @@ def convert_schema_to_markdown(schema_path: Path) -> str:
     markdown_content = f"# {schema.get('title', 'Title')}\n\n"
     markdown_content += f"## Overview\n\n{schema.get('description', 'No description available.')}\n\n"
 
+    # If this is a capability schema, add additional information
+    # this can be determined by looking at the _ref field
+    if '_ref' in schema:
+
+        if schema['_ref'].startswith('capability:'):
+
+            _frameworks = load_frameworks()
+            print(_frameworks)
+            return
+
+            # Add the framework information
+            frameworks = schema.get('frameworks', [])
+            if len(frameworks) > 0:
+                markdown_content += "## Frameworks\n"
+
+                for framework in frameworks:
+                    framework_name = "Unknown Framework"
+                    reference = []
+                    # Lookup the name of the framework in the loaded frameworks by
+                    # searching the friendly name
+                    if framework in _frameworks.keys():
+                        framework_name = _frameworks[framework].get(
+                            'title', framework)
+                        references = _frameworks[framework].get(
+                            'external_references', [])
+
+                    controls = frameworks[framework]
+                    markdown_content += f"### {framework_name}\n\n#### Controls\n\n"
+                    for control in controls:
+                        markdown_content += f"- **{control}**: \n"
+                    markdown_content += "\n"
+
+                    if references:
+                        markdown_content += "#### References\n\n"
+                        for reference in references:
+                            if reference['type'] == 'website':
+                                markdown_content += f"- [{reference['name']}]({reference['url']})\n"
+                            else:
+                                markdown_content += f"- {reference}\n"
+                        markdown_content += "\n"
+
     if 'fields' in schema:
         markdown_content += "## Fields\n"
         for field in schema['fields']:
@@ -22,14 +138,15 @@ def convert_schema_to_markdown(schema_path: Path) -> str:
 
     return markdown_content.strip()
 
+
 def main():
     """Main function to convert all schema JSON files in the current directory."""
-    
+
     OUTPUT_DIR = Path('build')
-    
+
     # Create the output directory if it doesn't exist
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-    
+
     # Locate the current working directory of python
     current_dir = Path(os.getcwd())
     print(f"Current directory: {current_dir}")
@@ -39,29 +156,30 @@ def main():
     print(f"Found {len(schema_files)} schema files to convert.")
 
     for schema_file in schema_files:
-        
+
         # Determine the folder structure for the markdown file about to be created
         relative_path = schema_file.relative_to(current_dir)
-        
+
         # Remove the 'spec' part of the path
         relative_path = relative_path.relative_to('spec')
-        
+
         # Replace the file extension with .md
         relative_path = relative_path.with_suffix('.md')
-        
+
         # Create the folder structure in the output directory
         # if it does not exist
         output_file_path = OUTPUT_DIR / relative_path
         output_file_path.parent.mkdir(parents=True, exist_ok=True)
-        
+
         # Convert the schema to markdown
         markdown_content = convert_schema_to_markdown(schema_file)
         markdown_file = schema_file.with_suffix('.md')
-        
+
         with open(output_file_path, 'w', encoding='utf-8') as md_file:
             md_file.write(markdown_content)
-        
+
         print(f"Converted {schema_file.name} to {markdown_file.name}")
-        
+
+
 if __name__ == "__main__":
     main()
